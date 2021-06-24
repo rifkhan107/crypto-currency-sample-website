9851471a7fc7a6530cf588c1bd6187c4a5c75370
Rifkhan Jm <53532191+rifkhan107@users.noreply.github.com>
2021-06-24 19:57:06 +0530

    Update index.js

--- src/Signin/index.js
+++ src/Signin/index.js
@@ -6,7 +6,7 @@ const SignIn = () => {
     <>
       <Container>
         <FormWrap>
-          <Icon to='/'>dolla</Icon>
+          <Icon to='/'>XeBeC Crypto</Icon>
           <FormContent>
             <Form action='#'>
               <FormH1>Sign in to your account</FormH1>
