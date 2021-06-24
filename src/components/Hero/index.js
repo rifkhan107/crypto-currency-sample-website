7c3f2db2bc26117dac1874f98171bb7d7276de7e
Rifkhan Jm <rifkhan561@gmail.com>
2021-06-24 17:40:25 +0530

    deploy to repo

---
+++ src/components/Hero/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import { Button } from "../ButtonElements";
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1> The world’s most popular way to buy, sell, and trade crypto</HeroH1>
+        <HeroP>
+        Trusted by millions since 2011 with over $800 Billion in crypto transactions.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-80}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
---
+++ src/components/Info/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Wallet",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg").default,
+  alt: "car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Login to your account at any time",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions whitout getting charged any fees.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/svg-4.svg").default,
+  alt: "Piggybanck",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Wallet",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions whitout getting charged any fees.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: require("../../images/svg-3.svg").default,
+  alt: "car",
+  primary: false,
+  darkText: true,
+};
